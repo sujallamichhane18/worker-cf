775e899db61b671fcf1f6c89f1bea150ffd8a64b
Sujal Lamichhane <141892300+sujallamichhane18@users.noreply.github.com>
2026-02-23 14:36:13 +0545

    Add Cloudflare Workers fetch handler

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Hello from Cloudflare Workers 🚀", {
+      headers: {
+        "content-type": "text/plain; charset=UTF-8",
+      },
+    });
+  },
+};
